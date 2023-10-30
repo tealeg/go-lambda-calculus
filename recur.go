bd9eddd90751b2e72a2e22b474a9ba9dad3a7b72
Geoffrey Teale <tealeg@gmail.com>
2023-10-30 13:57:01 +0100

    not really working Y-combinator

--- recur.go
+++ recur.go
@@ -3,10 +3,32 @@ package λ
 // This implementation is only a "candidate" - I haven't tested it yet
 
 // Y = λf.(λx.f (x x))(λx.f (x x))
-func Y(f λ) λ {
-	return func(x λ) λ {
-		return f(x(x))
-	}(func(x λ) λ {
-		return f(x(x))
-	})
+// func Y (f λ) λ {
+// 	return func (x λ) λ {
+// 		return f(x(x))
+// 	}(func(x λ) λ {
+// 		return f(x(x))
+// 	})
+// }
+
+func Y (f λ) λ {
+	return func(le λ) λ {
+		return func(g λ) λ{
+			return g(g)
+		}(
+			func(h λ) λ {
+				return le(func(x λ) λ {
+					return h(h)(x)
+				})
+			},
+		)
+	}
+}
+
+
+// F  = λ f. λ n. cond (isZero n) 1 (Mult n (f (Pred n))
+func F(f λ) λ {
+	return func(n λ) λ {
+		return IfThenElse(IsZero(n))(one)(mul(n)(f(pred(n))))
+	}
 }
--- recur_test.go
+++ recur_test.go
@@ -1,10 +1,26 @@
 package λ
 
-// import (
-// 	qt "github.com/frankban/quicktest"
-// 	"testing"
-// )
+import (
+	qt "github.com/frankban/quicktest"
+	"testing"
+)
 
-// func TestFib(t *testing.T) {
+func TestFact(t *testing.T) {
+	c := qt.New(t)
 
-// }
+	// Y returns the fixed point of F, which is the recursive factorial function
+	// fact := Y(F)
+	fact := Y(F)
+	c.Assert(fact, qt.IsNotNil)
+
+	// factorial of 0 is 1 (See here if you don't get why:
+	// https://www.thoughtco.com/why-does-zero-factorial-equal-one-3126598
+	// )
+	c.Assert(intResult(fact(zero)), qt.Equals, 1)
+
+	// factorial of 1 is 1
+	// This code will cause the program to panic - here we reach the limi t for Go
+	// c.Assert(intResult(fact(one)), qt.Equals, 1)
+
+	
+}
