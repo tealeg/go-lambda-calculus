1e0c9b35297477ff3cf25a41e356264153330e5a
Geoffrey Teale <tealeg@gmail.com>
2023-11-13 22:45:11 +0100

    Expand on scheme version a little

--- scheme-version/numbers.rkt
+++ scheme-version/numbers.rkt
@@ -42,8 +42,27 @@
 
 (define is-zero (lambda (a) (((a false) not) false)))
 
-(define y (lambda (f)
-            ((lambda (le) ((lambda (g) (g g))
+(define Y (lambda (f)
+            (lambda (le) ((lambda (g) (g g))
                            (lambda (h)
                              (le (lambda (x)
-                                   ((h h) x)))))))))
+                                   ((h h) x))))))))
+
+(define F (lambda (f) (lambda (n) (((if-then-else (is-zero n)) 1)
+	  			   ((mul n)(f (pred n)))))))
+
+(define fact (Y F))
+
+
+(define make-counter (lambda ()
+                       (define i 0)
+                       (define inc (lambda (f)
+                                     (set! i (+ i 1))))
+                       (define get (lambda () i))
+                       (define reset (lambda () (set! i 0)))
+                       (values inc get reset)))
+
+(let-values ([(inc get reset) (make-counter)])
+  (define res (fact zero))
+  (define val (res inc))
+  (print val))
