2e32abd0f0a68cac0c10656be5f6ff26d9e02dd7
Geoffrey J. Teale <tealeg@gmail.com>
2023-11-19 16:36:46 +0100

    Updates for 2023 presentation

--- recur.go
+++ recur.go
@@ -1,15 +1,5 @@
 package λ
 
-// This implementation is only a "candidate" - I haven't tested it yet
-
-// Y = λf.(λx.f (x x))(λx.f (x x))
-// func Y (f λ) λ {
-// 	return func (x λ) λ {
-// 		return f(x(x))
-// 	}(func(x λ) λ {
-// 		return f(x(x))
-// 	})
-// }
 
 func Y (f λ) λ {
 	return func(le λ) λ {
